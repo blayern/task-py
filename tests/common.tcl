5741b3c7be1bdd2aa219e79b32dca35b7d55e2e9
blayern <blayer@gmail.com>
2017-01-01 18:21:10 +0200

    proc logResult doesn't log to file anymore

--- tests/common.tcl
+++ tests/common.tcl
@@ -1,14 +1,10 @@
 #!/usr/bin/expect
 
-set timeNow [clock seconds]
-set logFile [open "./results/logfile-[clock format $timeNow -format %Y-%M-%d-%H-%M-%S].txt" w+]
-
 proc logResult {step result {msg {}}} {
 	global logFile
 	if {$step==""} {set str ""
 	} else {set str "Step "}
-	puts "$str$step: $result $msg"
-	puts $logFile "$str$step: $result $msg"
+	puts "$step: $result $msg"
 }
 
 proc createTestLine {args} {
